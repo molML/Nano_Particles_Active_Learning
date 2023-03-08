cea92ae74c3c5f61765243c80143a603307741b8
derekvantilborg <dwvantilborg@gmail.com>
2023-03-08 11:55:13 +0100

    Model evaluation function

--- nano/eval.py
+++ nano/eval.py
@@ -8,9 +8,47 @@ Derek van Tilborg | 06-03-2023 | Eindhoven University of Technology
 import numpy as np
 from nano.models import XGBoostEnsemble
 from nano.utils import augment_data
+import pandas as pd
 
 
-def k_fold_cross_validation(x: np.ndarray, y: np.ndarray, n_folds: int = 10, ensemble_size: int = 10, seed: int = 42,
+def evaluate_model(x: np.ndarray, y: np.ndarray, id: np.ndarray, filename: str, hyperparameters: dict,
+                   bootstrap: int = 10, n_folds: int = 5, ensemble_size=10, augment=5):
+    """ Function to evaluate model performance through bootstrapped k-fold cross-validation"""
+
+    # estimate model performance over b bootstraps
+    y_hats, y_hats_uncertainty = [], []
+    for b in range(bootstrap):
+        y_hat, y_hat_uncertainty = k_fold_cross_validation(x, y, seed=b,
+                                                           n_folds=n_folds,
+                                                           ensemble_size=ensemble_size,
+                                                           augment=augment,
+                                                           **hyperparameters)
+        y_hats.append(y_hat)
+        y_hats_uncertainty.append(y_hat_uncertainty)
+
+    # Take the mean over the bootstraps
+    mean_y_hat = np.mean(y_hats, axis=0)
+    mean_y_uncertainty = np.mean(y_hats_uncertainty, axis=0)
+
+    # Put everything in a dataframe and save it somewhere
+    df = pd.DataFrame({'ID': id,
+                       'PLGA': x[:, 0],
+                       'PP-L': x[:, 1],
+                       'PP-COOH': x[:, 2],
+                       'PP-NH2': x[:, 3],
+                       'S/AS': x[:, 4],
+                       'y': y,
+                       'y_hat': mean_y_hat,
+                       'y_uncertainty': mean_y_uncertainty})
+    df.to_csv(filename)
+
+    # calculate the RMSE
+    rmse = calc_rmse(y, mean_y_hat)
+
+    return df, rmse
+
+
+def k_fold_cross_validation(x: np.ndarray, y: np.ndarray, n_folds: int = 5, ensemble_size: int = 10, seed: int = 42,
                             augment: int = False, **kwargs) -> (np.ndarray, np.ndarray):
     assert len(x) == len(y), f"x and y should contain the same number of samples x:{len(x)}, y:{len(y)}"
 
