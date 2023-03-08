3e16c1f6caa784dd724f7a47f3a5f29f2511aec8
derekvantilborg <dwvantilborg@gmail.com>
2023-03-08 10:21:38 +0100

    nup data to Torch dataloader

--- nano/utils.py
+++ nano/utils.py
@@ -91,3 +91,11 @@ def augment_data(x: np.ndarray, y: np.ndarray, n_times: int = 10, shuffle: bool 
         y_prime = y_prime[shuffling]
 
     return x_prime, y_prime
+
+
+def numpy_to_dataloader(x: np.ndarray, y: np.ndarray = None, **kwargs) -> DataLoader:
+
+    if y is None:
+        return DataLoader(TensorDataset(Tensor(x)),  **kwargs)
+    else:
+        return DataLoader(TensorDataset(Tensor(x), Tensor(y)),  **kwargs)
