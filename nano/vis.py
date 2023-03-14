ebecae757bd71a0b8276c33c787088ed1569b80f
derekvantilborg <dwvantilborg@gmail.com>
2023-03-14 15:55:07 +0100

    Added PCA

--- nano/vis.py
+++ nano/vis.py
@@ -23,3 +23,27 @@ def scatter(y, y_hat, uncertainty=None, labels=None):
     ax.set_aspect(abs(x1-x0)/abs(y1-y0))
 
     plt.show()
+
+
+def picks_pca(screen_df, screen_x, picks):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from sklearn.decomposition import PCA
+
+    screen_df['pick'] = 0
+    screen_df.loc[screen_df['ID'].isin(picks), 'pick'] = 1
+    screen_df['alph'] = 0.01
+    screen_df.loc[screen_df['ID'].isin(picks), 'alph'] = 1
+
+    pca_screen = PCA(n_components=2)
+    pca_screen = pca_screen.fit_transform(screen_x)
+    pca_df = pd.DataFrame(data=pca_screen, columns=['PC 1', 'PC 2'])
+
+    plt.figure(figsize=(20, 20))
+    sns.scatterplot(
+        x="PC 1", y="PC 2",
+        hue=screen_df['pick'],
+        data=pca_df,
+        alpha=screen_df['alph']
+    )
+    plt.show()
