c778495f9e1eb7fcd05be15528a308d0a747849a
derekvantilborg <dwvantilborg@gmail.com>
2023-03-28 14:48:07 +0200

    Changed some filepaths

--- experiments/active_learning.py
+++ experiments/active_learning.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
 
     # hyperparameter optimization
     best_hypers_pdi = optimize_hyperparameters(x=x, y=pdi_y, std=pdi_std,
-                                               log_file=f'results/pdi_model_{CYCLE}_hypers_xgb_27Mar.csv',
+                                               log_file=f'results/pdi_model_{CYCLE}_hypers_xgb_27Mar_.csv',
                                                n_calls=HYPEROPT_CALLS_XGB,
                                                bootstrap=BOOTSTRAP,
                                                n_folds=N_FOLDS,
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 
     # Evaluate model performance with bootstrapped k-fold cross-validation
     pdi_eval_results, pdi_rmse = evaluate_model(x=x, y=pdi_y, std=pdi_std, id=id,
-                                                filename=f'results/pdi_model_{CYCLE}_eval_xgb_27Mar.csv',
+                                                filename=f'results/pdi_model_{CYCLE}_eval_xgb_27Mar_.csv',
                                                 hyperparameters=best_hypers_pdi,
                                                 bootstrap=BOOTSTRAP,
                                                 n_folds=N_FOLDS,
@@ -111,12 +111,12 @@ if __name__ == '__main__':
     pdi_x_augmented, pdi_y_augmented = augment_data(x, pdi_y, pdi_std, n_times=AUGMENT)
     pdi_model = XGBoostEnsemble(ensemble_size=1, **best_hypers_pdi)  # we use a single xgb model
     pdi_model.train(pdi_x_augmented, pdi_y_augmented)
-    torch.save(pdi_model, f'models/pdi_model_{CYCLE}_xgb.pt')
+    torch.save(pdi_model, f'models/pdi_model_{CYCLE}_xgb_27Mar_.pt')
 
     """ Screen ------------------------------------------------ """
 
-    uptake_model = torch.load(f'models/uptake_model_{CYCLE}_bnn.pt')
-    pdi_model = torch.load(f'models/pdi_model_{CYCLE}_xgb.pt')
+    uptake_model = torch.load(f'models/uptake_model_{CYCLE}_bnn_27Mar.pt')
+    pdi_model = torch.load(f'models/pdi_model_{CYCLE}_xgb_27Mar.pt')
 
     screen_df = screen_predict(screen_x, screen_id, uptake_model, pdi_model, f'results/screen_predictions_{CYCLE}.csv')
 
