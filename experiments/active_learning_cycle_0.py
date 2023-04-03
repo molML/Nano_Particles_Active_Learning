6a0dc86674c803443e64071571198597af14637c
derekvantilborg <dwvantilborg@gmail.com>
2023-04-03 09:38:21 +0200

    Renamed and added date field

--- experiments/active_learning_cycle_0.py
+++ experiments/active_learning_cycle_0.py
@@ -23,6 +23,7 @@ pd.set_option('display.max_columns', None)
 
 # Cycle
 CYCLE = 0
+DATE = '27Mar'
 # Number of tries during Bayesian hyperparameter optimization.
 HYPEROPT_CALLS_BNN = 100
 HYPEROPT_CALLS_XGB = 500
@@ -52,7 +53,7 @@ if __name__ == '__main__':
 
     # Hyperparameter optimization
     best_hypers_uptake = optimize_hyperparameters(x=x, y=uptake_y, std=uptake_std,
-                                                  log_file=f'results/uptake_model_{CYCLE}_hypers_bnn_27Mar.csv',
+                                                  log_file=f'results/uptake_model_{CYCLE}_hypers_bnn_{DATE}.csv',
                                                   n_calls=HYPEROPT_CALLS_BNN,
                                                   bootstrap=BOOTSTRAP,
                                                   n_folds=N_FOLDS,
@@ -62,7 +63,7 @@ if __name__ == '__main__':
 
     # Evaluate model performance with bootstrapped k-fold cross-validation
     uptake_eval_results, uptake_rmse = evaluate_model(x=x, y=uptake_y, std=uptake_std, id=id,
-                                                      filename=f'results/uptake_model_{CYCLE}_eval_bnn_27Mar.csv',
+                                                      filename=f'results/uptake_model_{CYCLE}_eval_bnn_{DATE}.csv',
                                                       hyperparameters=best_hypers_uptake,
                                                       bootstrap=BOOTSTRAP,
                                                       n_folds=N_FOLDS,
@@ -76,7 +77,7 @@ if __name__ == '__main__':
     uptake_x_augmented, uptake_y_augmented = augment_data(x, uptake_y, uptake_std, n_times=AUGMENT)
     uptake_model = BayesianNN(**best_hypers_uptake)
     uptake_model.train(uptake_x_augmented, uptake_y_augmented)
-    torch.save(uptake_model, f'models/uptake_model_{CYCLE}_bnn_27Mar.pt')
+    torch.save(uptake_model, f'models/uptake_model_{CYCLE}_bnn_{DATE}.pt')
 
     """ PdI model ------------------------------------------------ """
 
@@ -87,7 +88,7 @@ if __name__ == '__main__':
 
     # hyperparameter optimization
     best_hypers_pdi = optimize_hyperparameters(x=x, y=pdi_y, std=pdi_std,
-                                               log_file=f'results/pdi_model_{CYCLE}_hypers_xgb_27Mar_.csv',
+                                               log_file=f'results/pdi_model_{CYCLE}_hypers_xgb_{DATE}.csv',
                                                n_calls=HYPEROPT_CALLS_XGB,
                                                bootstrap=BOOTSTRAP,
                                                n_folds=N_FOLDS,
@@ -97,7 +98,7 @@ if __name__ == '__main__':
 
     # Evaluate model performance with bootstrapped k-fold cross-validation
     pdi_eval_results, pdi_rmse = evaluate_model(x=x, y=pdi_y, std=pdi_std, id=id,
-                                                filename=f'results/pdi_model_{CYCLE}_eval_xgb_27Mar_.csv',
+                                                filename=f'results/pdi_model_{CYCLE}_eval_xgb_{DATE}.csv',
                                                 hyperparameters=best_hypers_pdi,
                                                 bootstrap=BOOTSTRAP,
                                                 n_folds=N_FOLDS,
@@ -111,12 +112,12 @@ if __name__ == '__main__':
     pdi_x_augmented, pdi_y_augmented = augment_data(x, pdi_y, pdi_std, n_times=AUGMENT)
     pdi_model = XGBoostEnsemble(ensemble_size=1, **best_hypers_pdi)  # we use a single xgb model
     pdi_model.train(pdi_x_augmented, pdi_y_augmented)
-    torch.save(pdi_model, f'models/pdi_model_{CYCLE}_xgb_27Mar_.pt')
+    torch.save(pdi_model, f'models/pdi_model_{CYCLE}_xgb_{DATE}.pt')
 
     """ Screen ------------------------------------------------ """
 
-    uptake_model = torch.load(f'models/uptake_model_{CYCLE}_bnn_27Mar.pt')
-    pdi_model = torch.load(f'models/pdi_model_{CYCLE}_xgb_27Mar.pt')
+    uptake_model = torch.load(f'models/uptake_model_{CYCLE}_bnn_{DATE}.pt')
+    pdi_model = torch.load(f'models/pdi_model_{CYCLE}_xgb_{DATE}.pt')
 
     screen_df = screen_predict(screen_x, screen_id, uptake_model, pdi_model, f'results/screen_predictions_{CYCLE}.csv')
 
@@ -133,4 +134,4 @@ if __name__ == '__main__':
 
     # Write picks to dataframe
     picks_df = screen_df.loc[screen_df['ID'].isin(picks)]
-    picks_df.to_csv(f'results/picks_{CYCLE}.csv', index=False)
+    picks_df.to_csv(f'results/picks_{CYCLE}_{DATE}.csv', index=False)
