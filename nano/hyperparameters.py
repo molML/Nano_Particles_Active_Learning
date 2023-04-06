367cc089ad8b448af92474527ee9a6305158b11f
derekvantilborg <dwvantilborg@gmail.com>
2023-04-06 14:48:10 +0200

    Updated the hyperparameter search space

--- nano/hyperparameters.py
+++ nano/hyperparameters.py
@@ -5,10 +5,8 @@ XGBoost_hypers = {
         'max_depth': [2, 20],               # integer range from 2 - 20
         'min_child_weight': [1, 20],        # integer range from 1 - 20
         'gamma': [0.0, 10.0],               # float range from 0.0 - 10.0
-        'learning_rate': [0.001, 1.0],      # log-uniform float range from 0.0001 - 1.0
         'n_estimators': [50, 500],          # integer range from 50 - 500
         'eta': [0.1, 1.0],                  # float range from 0.1 - 1.0
-        'max_delta_step': [0, 10],          # integer range from 0 - 10
         'subsample': [0.1, 1.0],            # float range from 0.1 - 1.0
         'colsample_bytree': [0.1, 1.0],     # float range from 0.1 - 1.0
         'reg_alpha': [0.0, 10.0],           # float range from 0. - 10.0
@@ -19,7 +17,7 @@ XGBoost_hypers = {
     }
 
 
-BNN_hypers = {'lr': [1e-3, 1e-4, 1e-5],              # categorical
-              'hidden_size': [16, 32, 64],           # categorical
-              'epochs': [20000],                     # categorical
-              'n_layers': [3]}                       # integer range from 2 - 5
+BNN_hypers = {'lr': [1e-4, 1e-3, 1e-5],              # categorical
+              'hidden_size': [32],                   # categorical  64
+              'epochs': [10000, 20000],              # categorical
+              'n_layers': [2, 3]}                    # integer range from 2 - 5
